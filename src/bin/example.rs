90637c395004805b5bf895839b88888e1b32b642
Rémi Dupré <remi@dupre.io>
2022-12-01 21:32:43 +0100

    fix clippy lints

--- src/bin/example.rs
+++ src/bin/example.rs
@@ -65,7 +65,7 @@ mod day4 {
     }
 }
 
-aoc_main::main! {
+aoc::main! {
     year 2019;
     day1 : generator  => part_1, part_2;
     day2              => part_1_option?, part_1_result?, part_2_option?, part_2_result?;
